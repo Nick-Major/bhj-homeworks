5555682187c960eda42a0ed6cda0c696f1019f08
Nick-Major <divanchik26@gmail.com>
2024-04-06 23:36:22 +0300

    countdown done

---
+++ js-features/countdown/task.js
@@ -0,0 +1,13 @@
+let currentTime = 59;
+let timer = document.getElementById('timer');
+let counter = setInterval(countDown, 1000);
+
+function countDown() {
+    timer.textContent = currentTime;
+    currentTime--;
+    if (currentTime < 0) {
+        clearInterval(counter);
+        alert("Вы победили в конкурсе!");
+    }
+}
+
