e465c1064baf6a01369fc68d9995bfc5eb08a8aa
Nick-Major <divanchik26@gmail.com>
2024-05-09 22:52:05 +0300

    second commit todo

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,50 +1,26 @@
 const btn = document.getElementById('tasks__add');
 const input = document.getElementById('task__input');
 const tasksList = document.getElementById('tasks__list');
-const form = document.getElementById('tasks__form');
 btn.addEventListener('click', (event) => {
     event.preventDefault();
     addNewTask();
 })
 
 function addNewTask() {
-    const id = new Date().getTime();
-
-    let task = document.createElement('div');
-    task.classList.add('task');
-    task.setAttribute('id', id);
-
-    let taskTitle = document.createElement('div');
-    taskTitle.classList.add('task__title');
-
-    taskTitle.textContent = input.value;
-
-    let a = document.createElement('a');
-    a.classList.add('task__remove');
-    a.href = '#';
-    a.innerHTML = '&times;'
-    a.setAttribute('id', id);
-    a.onclick = function(event) {
-        removeTask(event);
-    };
-
-    task.appendChild(taskTitle);
-    task.appendChild(a);
-    tasksList.appendChild(task);
+    tasksList.insertAdjacentHTML("afterbegin", `
+    <div class="task">
+        <div class="task__title">
+            ${input.value}
+        </div>
+        <a href="#" class="task__remove">&times;</a>
+    </div>
+    `)
+    document.querySelectorAll('.task__remove').forEach((elem) => {
+        elem.addEventListener('click', removeTask);
+    })
+    input.value = '';
 }
 
 function removeTask(event) {
-    let tasks = document.querySelectorAll('.task');
-    console.log(tasks);
-
-    let id = event.target.id;
-
-    tasksList.textContent = '';
-    tasks.forEach((elem) => {
-        if (+elem.id === +id) {
-            return
-        }
-
-        tasksList.append(elem);
-    })
+    event.target.closest('.task').remove();
 }
