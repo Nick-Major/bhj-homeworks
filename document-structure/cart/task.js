ea8a16971a6705fc6d61954cd5a25fcd8d4e006a
Nick-Major <divanchik26@gmail.com>
2024-05-09 22:50:53 +0300

    second commit cart

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,38 +1,28 @@
-let cart = {
-    '1' : 1,
-    '2' : 1,
-    '3' : 1,
-    '4' : 1
-}
+const addFunc = (event) => {
+    const findId = event.target.closest('.product').dataset.id;
 
-document.onclick = (event) => {
-    let findId = event.target.closest('.product').dataset.id;
+    const basket = document.querySelector('.cart__products');
+    const product = Array.from(basket.querySelectorAll('.cart__product')).find((elem) => {
+        return elem.dataset.id === findId;
+    });
 
-    if (event.target.classList.contains('product__quantity-control_inc')) {
-        plusFunc(findId);
-        let countNumber = event.target.previousElementSibling;
-        countNumber.textContent++;
-    }
-
-    if (event.target.classList.contains('product__quantity-control_dec')) {
-        minusFunc(findId);
-        let countNumber = event.target.nextElementSibling;
-        countNumber.textContent--;
-        if (countNumber.textContent == 0) {
-            countNumber.textContent = 1;
-        }
+    if (product) {
+        const quantityValue = event.target.closest('.product').querySelector('.product__quantity-value');
+        product.lastElementChild.textContent = +product.lastElementChild.textContent + Number(quantityValue.textContent);
+        return;
     }
 
     if (event.target.classList.contains('product__add')) {
-        let idToCopy = event.target.closest('.product');
-        let cloneId = idToCopy.cloneNode(false);
+        const quantityValue = event.target.closest('.product').querySelector('.product__quantity-value');
+        const idToCopy = event.target.closest('.product');
+        const cloneId = idToCopy.cloneNode(false);
         cloneId.classList.replace('product', 'cart__product');
 
-        let imgToCopy = event.target.closest('.product__quantity').closest('.product__controls').previousElementSibling;
-        let clonedImg = imgToCopy.cloneNode(true);
+        const imgToCopy = event.target.closest('.product__quantity').closest('.product__controls').previousElementSibling;
+        const clonedImg = imgToCopy.cloneNode(true);
 
-        let cartProducts = document.querySelector('.cart__products');
-        let cartProductCount = document.createElement('div');
+        const cartProducts = document.querySelector('.cart__products');
+        const cartProductCount = document.createElement('div');
         cartProductCount.classList.add('cart__product-count');
 
         if (!cartProducts.querySelector(`.cart__product[data-id="${findId}"]`)) {
@@ -40,27 +30,32 @@ document.onclick = (event) => {
             cloneId.insertAdjacentElement('afterBegin', clonedImg);
             cloneId.insertAdjacentElement('beforeEnd', cartProductCount);
 
-            let countNumber = idToCopy.querySelector('.product__quantity-value');
-            cartProductCount.textContent = countNumber.textContent;
+            cartProductCount.textContent = quantityValue.textContent;
         }
-
-        let itemNumber = cloneId.dataset.id;
-        console.log(itemNumber);
-
-        console.log(cart);
     }
 
 }
 
-const plusFunc = (id) => {
-    cart[id]++;
-    console.log(cart);
+const plusFunc = (event) => {
+    const value = event.target.previousElementSibling;
+    value.textContent++;
 }
 
-const minusFunc = (id) => {
-    cart[id]--;
-    if (cart[id] === 0) {
-        cart[id] = 1;
+const minusFunc = (event) => {
+    const value = event.target.nextElementSibling;
+    if (value.textContent > 1) {
+        value.textContent--;
     }
-    console.log(cart);
 }
+
+
+const productsElCollection = document.querySelectorAll('.product');
+productsElCollection.forEach((elem) => {
+    const plus = elem.querySelector('.product__quantity-control_inc');
+    const minus = elem.querySelector('.product__quantity-control_dec');
+    const add = elem.querySelector('.product__add');
+
+    plus.addEventListener('click', plusFunc);
+    minus.addEventListener('click', minusFunc);
+    add.addEventListener('click', addFunc);
+})
