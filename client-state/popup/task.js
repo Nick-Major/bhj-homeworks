d687e52a9b33d4a0c3528da933dee73c030a0e64
Nick-Major <divanchik26@gmail.com>
2024-05-19 12:05:04 +0300

    first commit popup

---
+++ client-state/popup/task.js
@@ -0,0 +1,43 @@
+const modal = document.getElementById('subscribe-modal');
+const modalСlose = document.querySelector('.modal__close');
+
+if (getCookie('windowState') !== 'close') {
+    modal.classList.add('modal_active');
+}
+
+modal.classList.add('modal_active');
+modalСlose.addEventListener('click', () => {
+    modal.classList.remove('modal_active');
+    setCookie('windowState', 'close', {'max-age': 3600});
+})
+
+function setCookie(name, value, options = {}) {
+
+  options = {
+    path: '/',
+    ...options
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
